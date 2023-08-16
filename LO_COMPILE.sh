5c85f36ded300e540e0f4ac04544bf11f55eed57
Patrick Tripp <44276748+patrick-tripp@users.noreply.github.com>
2023-08-16 17:09:32 -0400

    Newlo (#1)
    * LiveOcean builds and runs - avx512
    
    * adding AMD build support
    
    * haswell and x86_64 target stubs
    
    * preparing for x86_64 support

--- LO_COMPILE.sh
+++ LO_COMPILE.sh
@@ -1,36 +1,75 @@
 #!/bin/bash
+#set -x
 
-modconf=lo6biom
 TOPDIR=$PWD
 
-makedir=$TOPDIR/LiveOcean_roms/makefiles/$modconf
-makefile=$makedir/makefile
+BUILDDIR="${TOPDIR}/LO_roms_user/x2b"
+BUILDSCRIPT=build_roms.sh
+BUILDOPTS='-j 2'
 
-ROMSDIR=$TOPDIR/LiveOcean_roms/LO_ROMS
-EXECDIR=$TOPDIR/LiveOcean_roms/exec
-mkdir -p $EXECDIR
+# BUILDOPTS
+#    -j [N]      Compile in parallel using N CPUs                       :::
+#                  omit argument for all available CPUs                 :::
+#                                                                       :::
+#    -p macro    Prints any Makefile macro value. For example,          :::
+#                                                                       :::
+#                  build_roms.sh -p FFLAGS                              :::
+#                                                                       :::
+#    -noclean    Do not clean already compiled objects 
 
-export COMP_F=gfortran
+export MY_ROOT_DIR=/save/ioos/patrick.tripp/LiveOcean
+export MY_ROMS_SRC=${MY_ROOT_DIR}/LO_roms_source
+
+# Using a different makefile for Sandbox
+cp -p ./Compilers/x2b/Linux-ifort.mk $MY_ROMS_SRC
+
+export COMP_F=ifort
 export COMP_F_MPI90=mpif90
-export COMP_F_MPI=mpifc
-export COMP_ICC=gcc
-export COMP_CC=gcc
+export COMP_F_MPI=mpif90
+export COMP_ICC=icc
+export COMP_CC=icc
 export COMP_CPP=cpp
-export COMP_MPCC=mpigcc
+export COMP_MPCC='mpicc -fc=icc'
+
+# Compiler target machine
+TARGETMX=${TARGETMX:-'skylake_avx512'}
+# TARGETMX='haswell'
 
 module purge
-module load gcc/6.5.0
-#module load mpi/intel/2019.5.281
-#module load mpi/intel/2020.0.154
-module load mpi/intel/2020.0.166
-# module load netcdf/4.2
-module load netcdf/4.5
 
-export NETCDF_INCDIR="$NETCDF/include"
-export NETCDF_LIBDIR="$NETCDF/lib"
-export NETCDF_LDFLAGS="-L$NETCDF_LIBDIR -lnetcdff -lnetcdf"
+if [[ $TARGETX == "skylake_avx512" ]]; then
 
-cd $ROMSDIR
-#gmake clean
-gmake -f $makefile
-mv $makedir/oceanM $EXECDIR/oceanM.$modconf
+  # don't set NO_AVX512 - make uses ifdef - default is to use
+  module load gcc-8.5.0-gcc-4.8.5-iakdnjp
+  module load intel-oneapi-compilers-2021.3.0-gcc-8.5.0-gp3iweu
+  module load intel-oneapi-mpi-2021.3.0-intel-2021.3.0-bixgqcx
+  module load zlib-1.2.11-intel-2021.3.0-gcfkvht
+  module load libszip-2.1.1-intel-2021.3.0-pzpdymm
+  module load netcdf-c-4.8.0-intel-2021.3.0-pejnxdb
+  module load netcdf-fortran-4.5.4-intel-2021.3.0-4lyzqsf
+  module load hdf5-1.10.7-intel-2021.3.0-btc4zhc
+
+elif [[ $TARGETMX == "haswell" ]]; then
+  export NO_AVX512=on
+  module avail
+  echo "target haswell not supported yet"
+  exit
+elif [[ $TARGETMX == "x86_64" ]]; then
+  # Use the x86_64 build targets
+  export NO_AVX512=on
+  module avail
+  echo "target x86_64 not supported yet"
+  exit
+else
+  echo "Target platform not supported"
+  exit
+fi
+
+exit
+
+NETCDF=`nf-config --prefix`
+export NETCDF_INCDIR=`nf-config --includedir`
+export NETCDF_LIBDIR="${NETCDF}/lib"
+cd $BUILDDIR
+./$BUILDSCRIPT $BUILDOPTS
+
--- jobs/testPWD.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "I am in $PWD"
-export HOMEnos=$(dirname $PWD)
-echo "HOMEnos is $HOMEnos"
-
